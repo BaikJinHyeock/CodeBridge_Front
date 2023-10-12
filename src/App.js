3d7ea558b6f8d4f077b40e0cbf12468d3e2c5f3e
최수환 <frcp9408@gmail.com>
2023-10-12 14:37:56 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Test />}></Route>
+
+      </Routes>
 
     </div>
   );
