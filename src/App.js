7225f5bf3fb56a76be56daf84e291b7315c3bbae
BaikJinHyeock <me335097@gmail.com>
2023-10-12 16:03:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Test from './components/Test';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Test from "./components/Test";
+import studyTest from "./components/studyTest";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Test />}></Route>
-
+        {/* <Route path="/" element={<Test />}></Route> */}
+        <Route path="/" element={<studyTest />}></Route>
       </Routes>
-
     </div>
   );
 }
