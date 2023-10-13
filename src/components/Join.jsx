6cd9b888edb35fc1a366b6aac972889102b3c76c
donguk717 <donguk717@gmail.com>
2023-10-13 12:11:46 +0900

    선동욱_join 중간커밋

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,19 +1,38 @@
+import { async } from "q";
 import React from "react";
 
 const Join = () => {
+
+  const JoinMember = async (e) => {
+    e.preventDefault();
+    let member = {
+      user_id: id,
+      user_pw: passowrd,
+      user_name: name1,
+      user_nick: nick,
+      user_phone: phone,
+      user_type: type
+    };
+    try{
+      const response = await axois.post("http://localhost:8085/MemberJoin")
+    }
+  }
+
+
   return (
     <div>
-      <form>
+      <form onSubmit={JoinMember}>
         <h1>JoinPage</h1>
-        <input type="email" placeholder="email"></input> <br />
-        <input type="password" placeholder="password"></input> <br />
-        <input type="text" placeholder="Your name"></input> <br />
-        <input type="number" placeholder="Your number"></input> <br />
+        <input type="email" placeholder="email" value="id"></input> <br />
+        <input type="password" placeholder="password" value="pw"></input> <br />
+        <input type="text" placeholder="Your name" value="name1"></input> <br />
+        <input type="text" placeholder="Your nickname" value="nick"></input> <br />
+        <input type="number" placeholder="Your number" value="phone"></input> <br />
         <input type="text" placeholder="Nickname"></input> <br />
-        <select>
-          <option value="none">학생 or 선생 타입선택</option>
-          <option value="student">Student</option>
-          <option value="teacher">Teacher</option>
+        <select name="type">
+          <option value="type">학생 or 선생 타입선택</option>
+          <option value="1">Student</option>
+          <option value="0">Teacher</option>
         </select>
         <br />
         <button type="submit">로그인</button>
@@ -21,5 +40,7 @@ const Join = () => {
     </div>
   );
 };
+
+
 
 export default Join;
