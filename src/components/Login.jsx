d7eab57461563782e62c37a5e5d3454853cfc952
BaikJinHyeock <me335097@gmail.com>
2023-10-13 15:55:59 +0900

    백진혁_ClassWrite페이지 작성

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import style from "../SCSS/pages/_login.module.scss";
+
 const Login = () => {
   return (
-    <div>
-      <h1>LoginPage</h1>
-      <input type="text" placeholder="id"></input> <br />
-      <input type="password" placeholder="password"></input> <br />
-      <button type="submit">로그인</button>
+    <div className={style.wrap_container}>
+      <div className={style.input_box}>
+        <h1>Log in to Code Bridge</h1>
+        <input type="text" placeholder="id"></input> <br />
+        <input type="password" placeholder="password"></input> <br />
+        <button type="submit">로그인</button>
+        <div className={style.division_line}>
+          <div>
+            <span>OR</span>
+          </div>
+        </div>
+        <button type="submit">회원가입</button>
+      </div>
     </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import main from "../SCSS/pages/_main.module.scss";
+import style from "../SCSS/pages/_main.module.scss";
 
 const Main = () => {
   return (
-    <div className={main.wrap_container}>
+    <div className={style.wrap_container}>
       <button>
         <Link to={"Login"}>로그인</Link>
       </button>
