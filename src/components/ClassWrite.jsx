465c77a685014933734400befef375d9d1de9864
BaikJinHyeock <me335097@gmail.com>
2023-10-13 19:38:28 +0900

    백진혁_강의실생성

--- src/components/ClassWrite.jsx
+++ src/components/ClassWrite.jsx
@@ -2,65 +2,118 @@ import axios from "axios";
 import React, { useState } from "react";
 
 const ClassWrite = () => {
-
   const [title, setTitle] = useState("");
-  const [lang, setLang] = useState("");
-  const [intro, setIntro] = useState("");
-  const [goal, setGoal] = useState("");
+  const [subject, setSubject] = useState("");
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
-  const [teacher_intro, setTeacher_intro] = useState("");
+  const [content, setContent] = useState("");
+  const [week, setWeek] = useState("");
+  const [weekContent, setWeekContent] = useState("");
+  const [weekList, setWeekList] = useState([0]);
 
-  const Class = async (e) => {
-    e.preventDefault();
-    let class = {
-      class_title: title,
-      class_lang : lang,
-      class_intro: intro,
-      teacher_intro: teacher_intro,
-      class_goal: goal,
-      class_start: start,
-      class_end: end
-    };
-    const response = await axios.post("http://localhost:8085/CodeBridge/MemberLogin.do", class);
-    console.log('리스폰스 확인', response);
-  }
+  const List = () => (
+    <div>
+      {weekList.map((item, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            value={item.week}
+            placeholder="주차"
+            onChange={(e) => handleWeekChange(e, index)}
+          ></input>
+          <input
+            type="text"
+            value={item.weekContent}
+            placeholder="주차 별 내용"
+            onChange={(e) => handleWeekContentChange(e, index)}
+          ></input>
+        </div>
+      ))}
+    </div>
+  );
+
+  const CreateList = () => {
+    setWeekList([...weekList, { week, weekContent }]);
+    setWeek(""); // Clear the week input
+    setWeekContent(""); // Clear the weekContent input
+  };
+
+  // Update the week at the specified index
+  const handleWeekChange = (e, index) => {
+    const updatedList = [...weekList];
+    updatedList[index].week = e.target.value;
+    setWeekList(updatedList);
+  };
+
+  // Update the weekContent at the specified index
+  const handleWeekContentChange = (e, index) => {
+    const updatedList = [...weekList];
+    updatedList[index].weekContent = e.target.value;
+    setWeekList(updatedList);
+  };
+
   return (
     <div>
-      <form onSubmit={Class}>
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}>강의명</input>
-        <input type="checkbox" name="lang" value={java}>자바</input>
-        <input type="checkbox" name="lang" value={javascript}>자바스크립트</input>
-        <input type="checkbox" name="lang" value={python}>파이썬</input>
-        <input type="checkbox" name="lang" value={HTML/CSS}>HTML/CSS</input>
-        <input type="checkbox" name="lang" value={jsp/servlet}>jsp/servlet</input>
-        <input type="checkbox" name="lang" value={react}>리엑트</input>
-        <input type="checkbox" name="lang" value={spring}>스프링</input>
-        <input type="checkbox" name="lang" value={database}>데이터베이스</input>
-        <input type="checkbox" name="lang" value={golang}>Golang</input>
-        <input type="checkbox" name="lang" value={erlng}>Erlang</input>
-        <input type="checkbox" name="lang" value={machinelearning}>머신러닝</input>
-        <input type="checkbox" name="lang" value={deeplearning}>딥러닝</input>
-        <input type="checkbox" name="lang" value={spring}>스프링</input>
-        <input type="checkbox" name="lang" value={C샵}>C샵</input>
-        <input type="checkbox" name="lang" value={C++}>C++</input>
-        <input type="checkbox" name="lang" value={C}>C</input>
-        <input type="checkbox" name="lang" value={php}>PHP</input>
-        <input type="checkbox" name="lang" value={ruby}>루비</input>
-        <input type="checkbox" name="lang" value={Haskell}>Haskell</input>
-        <input type="checkbox" name="lang" value={Smalltalk}>Smalltalk</input>
-        <input type="checkbox" name="lang" value={Pascal}>Pascal</input>
-        <input type="checkbox" name="lang" value={Lisp}>Lisp</input>
-        <input type="checkbox" name="lang" value={Ada}>Ada</input>
-        <input type="checkbox" name="lang" value={Lua}>Lua</input>
-        <input type="checkbox" name="lang" value={Prolog}>Prolog</input>
-        <input type="checkbox" name="lang" value={node}>node</input>
-        <input type="text" value={intro} onChange={(e) => setIntro(e.target.value)}>강의설명글</input>
-        <input type="text" value={goal} onChange={(e) => setGoal(e.target.value)}>강의목표</input>
-        <input type="date" value={start} onChange={(e) => setStart(e.target.value)}>강의개설일</input>
-        <input type="date" value={end} onChange={(e) => setEnd(e.target.value)}>강의종료일</input>
-        <input type="date" value={teacher_intro} onChange={(e) => setTeacher_intro(e.target.value)}>강사소개</input>
-        <button type="submit">강의개설</button>
+      <form>
+        <input
+          type="text"
+          value={title}
+          placeholder="제목"
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <br />
+        <br />
+        <input
+          type="text"
+          value={subject}
+          placeholder="교육대상"
+          onChange={(e) => setSubject(e.target.value)}
+        ></input>
+        <br />
+        <br />
+
+        <input
+          type="date"
+          value={start}
+          onChange={(e) => setStart(e.target.value)}
+        ></input>
+
+        <br />
+        <br />
+        <input
+          type="date"
+          value={end}
+          onChange={(e) => setEnd(e.target.value)}
+        ></input>
+
+        <br />
+        <br />
+        <input
+          type="text"
+          value={content}
+          placeholder="교육설명"
+          onChange={(e) => setContent(e.target.value)}
+        ></input>
+
+        <br />
+        <br />
+        <div>
+          <List />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-plus-circle-fill"
+            viewBox="0 0 16 16"
+            onClick={CreateList}
+          >
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+          </svg>
+        </div>
+        <hr />
+
+        <button type="submit">교육개설</button>
       </form>
     </div>
   );
