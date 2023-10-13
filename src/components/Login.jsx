f7b1c860690f2957177803ae9681f1cdd71343c5
BaikJinHyeock <me335097@gmail.com>
2023-10-13 17:37:26 +0900

    백진혁_로그인 페이지 완료

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 
 import style from "../SCSS/pages/_login.module.scss";
+// import "../main.css";
 
 const Login = () => {
   const [id, setId] = useState("");
@@ -11,29 +12,46 @@ const Login = () => {
     e.preventDefault();
     let member = {
       user_id: id,
-      user_pw: password
+      user_pw: password,
     };
-    const response = await axios.post("http://localhost:8085/CodeBridge/MemberLogin.do", member);
-    console.log('리스폰스 확인', response);
-  }
+    const response = await axios.post(
+      "http://localhost:8085/CodeBridge/MemberLogin.do",
+      member
+    );
+    console.log("리스폰스 확인", response);
+  };
   return (
     <div className={style.wrap_container}>
       <div className={style.input_box}>
         <form onSubmit={LoginMember}>
           <h1>Log in to Code Bridge</h1>
-          <input type="text" placeholder="id" value={id} onChange={(e) => setId(e.target.value)}></input>
-          <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-          <button type="submit">로그인</button>
+          <input
+            type="text"
+            placeholder="ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type="submit" className={style.login_button}>
+            로그인
+          </button>
           <div className={style.division_line}>
             <div>
               <span>OR</span>
             </div>
           </div>
-          <button type="submit">회원가입</button>
+          <button type="submit" className={style.join_button}>
+            회원가입
+          </button>
         </form>
       </div>
     </div>
-      );
+  );
 };
 
-      export default Login;
+export default Login;
