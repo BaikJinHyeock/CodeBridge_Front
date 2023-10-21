fa09ca616f3344d25edd59f12be8d7047d6fa531
SooHwanC <frcp9408@gmail.com>
2023-10-21 17:51:34 +0900

    최수환_임시커밋

---
+++ src/components/QuillCompo_test.jsx
@@ -0,0 +1,60 @@
+import React, { useMemo, useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useDispatch, useSelector } from "react-redux";
+import { updateQuillValue } from "../actions/quillActions";
+import "../css/Quill.css";
+
+
+const QuillCompo_test = ({ update }) => {
+
+    const dispatch = useDispatch();
+
+    const handleQuillChange = (value) => {
+        dispatch(updateQuillValue(value));
+    }
+
+    const quillValue = useSelector((state) => state.quill.quillValue);
+
+
+    const quillRef = useRef(null); // useRef로 ref 생성
+
+
+    const modules = useMemo(() => {
+        return {
+            toolbar: {
+                container: [
+                    [{ header: [1, 2, 3, false] }],
+                    ["bold", "italic", "underline", "strike", "blockquote"],
+                    [{ color: [] }, { background: [] }],
+                ],
+            },
+        };
+    }, []);
+
+    const formats = [
+        "header",
+        "bold",
+        "italic",
+        "underline",
+        "strike",
+        "blockquote",
+    ];
+
+    return (
+        <div>
+            <ReactQuill
+                style={{ height: "400px" }}
+                ref={quillRef} // useRef로 생성한 ref를 연결
+                theme="snow"
+                placeholder="내용을 입력해주세요."
+                value={quillValue}
+                onChange={handleQuillChange}
+                modules={modules}
+                formats={formats}
+            />
+        </div>
+    );
+};
+
+export default QuillCompo_test;
