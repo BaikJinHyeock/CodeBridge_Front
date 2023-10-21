6ffe5363523c88c7828d3072a9f90655ea5b8f10
BaikJinHyeock <me335097@gmail.com>
2023-10-21 12:44:41 +0900

    백진혁_테스트 완성

--- src/components/TestList.jsx
+++ src/components/TestList.jsx
@@ -1,44 +1,36 @@
-import React from 'react'
+import React from "react";
 import style from "../SCSS/pages/_testList.module.scss";
-import Title from './Title';
-import Profile from './Profile';
+import Title from "./Title";
+import Profile from "./Profile";
 
 const TestList = () => {
-
-    const TestItem = () => {
-
-
-        return (
-            <div className={style.test_item_box}>
-                <div>
-                    <span>1주차</span>
-                    <span>쉽게 배우는 Java수업</span>
-                </div>
-                <div className={style.test_btn}>
-                    테스트
-                </div>
-            </div>
-        );
-    }
-
+  const TestItem = () => {
     return (
-        <div className={style.wrap_container}>
-            <div className={style.right_container}>
-                <Title pageName="테스트" />
-                <Profile showEditButton={false} />
-            </div>
-
-            <h2>테스트 관리</h2>
-            <div className={style.test_item_wrapper}>
-                <TestItem />
-                <TestItem />
-                <TestItem />
-            </div>
-
-
-
+      <div className={style.test_item_box}>
+        <div>
+          <span>1주차</span>
+          <span>쉽게 배우는 Java수업</span>
         </div>
-    )
-}
+        <div className={style.test_btn}>테스트</div>
+      </div>
+    );
+  };
 
-export default TestList
+  return (
+    <div className={style.wrap_container}>
+      <div className={style.right_container}>
+        {/* <Title pageName="테스트" /> */}
+        <Profile showEditButton={false} />
+      </div>
+
+      <h2>테스트 관리</h2>
+      <div className={style.test_item_wrapper}>
+        <TestItem />
+        <TestItem />
+        <TestItem />
+      </div>
+    </div>
+  );
+};
+
+export default TestList;
