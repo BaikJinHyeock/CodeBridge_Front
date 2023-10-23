c92abf577388d1823bd679f63a263dd94b6bdd88
BaikJinHyeock <me335097@gmail.com>
2023-10-23 14:42:29 +0900

    백진혁_메인 완료

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,19 +3,20 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import { FreeMode } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import ScrollTrigger from "@terwanerik/scrolltrigger";
 
 import style from "../SCSS/pages/_main.module.scss";
+import { Container } from "./styled";
+import { useScrollAnimation } from "./useScrollAnimation";
 
 const Main = () => {
-  useEffect(() => {
-    const trigger = new ScrollTrigger();
-    trigger.add("[data-trigger]", {
-      onEnter: (element) => {
-        element.classList.add("animated"); // 애니메이션을 위한 CSS 클래스 추가
-      },
-    });
-  }, []);
+  const Scroll = ({ children }) => {
+    const { ref, isInViewport } = useScrollAnimation();
+    return (
+      <Container ref={ref} className={isInViewport ? "frame-in" : ""}>
+        {children}
+      </Container>
+    );
+  };
 
   const ClassContent = () => {
     return (
@@ -83,100 +84,106 @@ const Main = () => {
           </Swiper>
         </div>
 
-        <div className={style.main_grid} data-trigger>
-          <div className={style.main_grid_leftBox}>
-            <h1>
-              <span>LMS</span>에서 제작한
-              <br /> 온라인 코딩 에듀케이션
-            </h1>
-            <div>
-              <h2>Code Bridge</h2>
-              <span>
-                프로젝트 바로가기
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-arrow-right-circle-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                </svg>
-              </span>
+        <Scroll>
+          <div className={style.main_grid}>
+            <div className={style.main_grid_leftBox}>
+              <h1>
+                <span>LMS</span>에서 제작한
+                <br /> 온라인 코딩 에듀케이션
+              </h1>
+              <div>
+                <h2>Code Bridge</h2>
+                <span>
+                  프로젝트 바로가기
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-arrow-right-circle-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div className={style.main_grid_rightBox}>
+              <img
+                src="https://i.ibb.co/jTCKSBy/Amigos-Home-Alone.png"
+                alt="mainImage1"
+              />
             </div>
           </div>
+        </Scroll>
 
-          <div className={style.main_grid_rightBox}>
-            <img
-              src="https://i.ibb.co/jTCKSBy/Amigos-Home-Alone.png"
-              alt="mainImage1"
-            />
-          </div>
-        </div>
-
-        <div className={style.main_grid} data-trigger>
-          <div className={style.main_grid_rightBox}>
-            <img
-              src="https://i.ibb.co/Rbg7kD5/Amigos-Online-Shopping.png"
-              alt="#"
-            />
-          </div>
-          <div className={style.main_grid_leftBox}>
-            <h1>
-              <span>스마트인재개발</span>에서 하는 강의를
-              <br /> Code Bridge로 간편하게
-            </h1>
-            <div>
-              <h2>To be together</h2>
-              <span>
-                회원가입 바로가기
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-arrow-right-circle-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                </svg>
-              </span>
+        <Scroll>
+          <div className={style.main_grid}>
+            <div className={style.main_grid_rightBox}>
+              <img
+                src="https://i.ibb.co/Rbg7kD5/Amigos-Online-Shopping.png"
+                alt="#"
+              />
+            </div>
+            <div className={style.main_grid_leftBox}>
+              <h1>
+                <span>스마트인재개발</span>에서 하는 강의를
+                <br /> Code Bridge로 간편하게
+              </h1>
+              <div>
+                <h2>To be together</h2>
+                <span>
+                  회원가입 바로가기
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-arrow-right-circle-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                  </svg>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
+        </Scroll>
 
-        <div className={style.main_grid} data-trigger>
-          <div className={style.main_grid_leftBox}>
-            <h1>
-              팀원들과 함께하는 <span>페어코딩,</span>
-              <br /> 코딩작업과 소통을 한번에!
-            </h1>
-            <div>
-              <h2>Let's Develop</h2>
-              <span>
-                내 강의실 바로가기
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-arrow-right-circle-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                </svg>
-              </span>
+        <Scroll>
+          <div className={style.main_grid}>
+            <div className={style.main_grid_leftBox}>
+              <h1>
+                팀원들과 함께하는 <span>페어코딩,</span>
+                <br /> 코딩작업과 소통을 한번에!
+              </h1>
+              <div>
+                <h2>Let's Develop</h2>
+                <span>
+                  내 강의실 바로가기
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-arrow-right-circle-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div className={style.main_grid_rightBox}>
+              <img
+                src="https://i.ibb.co/GP8WTz3/Amigos-Chatting.png"
+                alt="mainImage1"
+              />
             </div>
           </div>
-
-          <div className={style.main_grid_rightBox}>
-            <img
-              src="https://i.ibb.co/GP8WTz3/Amigos-Chatting.png"
-              alt="mainImage1"
-            />
-          </div>
-        </div>
+        </Scroll>
       </div>
     </div>
   );
