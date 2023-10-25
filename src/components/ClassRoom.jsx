9b163e33c71836b7dae8b431e936a19a08b0b5d5
BaikJinHyeock <me335097@gmail.com>
2023-10-25 09:49:29 +0900

    백진혁_클래스룸완료

--- src/components/ClassRoom.jsx
+++ src/components/ClassRoom.jsx
@@ -4,11 +4,16 @@ import style from "../SCSS/pages/_classRoom.module.scss";
 export const ClassRoom = () => {
   return (
     <div className={style.main_container}>
-      <h1>CodeBridge VSCODE 테스트</h1>
-      <iframe src="http://59.0.234.207:8083/?folder=/home/smhrd/test">
-      </iframe>
-
-
+      <iframe src="http://59.0.234.207:8083/?folder=/home/smhrd/test"></iframe>
+      <div className={style.main_container_right}>
+        <div className={style.main_container_right_buttons}>
+          <button type="button">선생님 화면 보러가기</button>
+          <button type="button">도움요청</button>
+        </div>
+        <div className={style.main_container_right_chat}>
+            채팅화면
+        </div>
+      </div>
     </div>
   );
 };
