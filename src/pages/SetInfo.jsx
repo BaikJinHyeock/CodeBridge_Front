54178331d76415304d61b6d016710da30374caa9
BaikJinHyeock <me335097@gmail.com>
2023-11-03 14:25:55 +0900

    백진혁_이름변경 삭제

--- src/pages/SetInfo.jsx
+++ src/pages/SetInfo.jsx
@@ -57,34 +57,34 @@ const SetInfo = () => {
     console.log('인포리스트 확인', infoList.user_name);
     
     const koreanVowelRegex = /^[ㅏ-ㅣ]/;
-  const namecheck = async (e) => {
-    if (name1.length < 2 || name1.length > 5 || koreanVowelRegex.test(name1[0])) {
-      setNameCheckMsg("이름을 정확하게 입력해주세요");
-      setCheck5(0);
-    } else {
-      setNameCheckMsg("");
-      setCheck5(1);
-    }
-  };
-  const name_deit = async (e) => {
-    e.preventDefault();
-    if (check5 === 1) {
-      let mem = {
-        user_id: id,
-        user_name: name1,
-      };
-      const response = await axios.post(
-        "http://localhost:8085/CodeBridge/member/nameedit",
-        mem
-      );
-      if (response.data === 1) {
-        window.location.href = "/";
-        alert("이름이 성공적으로 수정되었습니다.");
-      }
-    } else {
-      return alert("잘못입력된 정보가 있습니다.");
-    }
-  };
+  // const namecheck = async (e) => {
+  //   if (name1.length < 2 || name1.length > 5 || koreanVowelRegex.test(name1[0])) {
+  //     setNameCheckMsg("이름을 정확하게 입력해주세요");
+  //     setCheck5(0);
+  //   } else {
+  //     setNameCheckMsg("");
+  //     setCheck5(1);
+  //   }
+  // };
+  // const name_deit = async (e) => {
+  //   e.preventDefault();
+  //   if (check5 === 1) {
+  //     let mem = {
+  //       user_id: id,
+  //       user_name: name1,
+  //     };
+  //     const response = await axios.post(
+  //       "http://localhost:8085/CodeBridge/member/nameedit",
+  //       mem
+  //     );
+  //     if (response.data === 1) {
+  //       window.location.href = "/";
+  //       alert("이름이 성공적으로 수정되었습니다.");
+  //     }
+  //   } else {
+  //     return alert("잘못입력된 정보가 있습니다.");
+  //   }
+  // };
   
   const nickcheck = async (e) => {
     if (nick.length < 2 || nick.length > 10 || koreanVowelRegex.test(nick[0]) ) {
@@ -204,15 +204,15 @@ const SetInfo = () => {
         alert("성공적으로 삭제되었습니다.");
       }
     } else {
-      return alert("회원삭제실패!");
+      return ;
     }
   };
 
   // 수정버튼 클릭시 div박스 display : flex 변경함수
   const [onButton, setOnButton] = useState("");
-  const onId = () => {
-    setOnButton("userId");
-  };
+  // const onId = () => {
+  //   setOnButton("userId");
+  // };
   const onNick = () => {
     setOnButton("userNick");
   };
@@ -343,35 +343,6 @@ const SetInfo = () => {
     <div className={style.wrap_container}>
       <div className={style.left_container}>
         <div className={style.profile_box_after}>
-
-          {/*           <div className={style.profile_wrap_container}>
-            <div className={style.profile_img}>
-              <img
-                src="https://mblogthumb-phinf.pstatic.net/MjAyMTAzMjJfMjkg/MDAxNjE2Mzg4ODI0NzI5.uBHIwocqtEiKlHbUpds05YCDMe6Arw0o_l-p3PdJFZEg.GqEQvSTGKySHJrOTOE2nLGnlbZx3Cb9xfllMFlCRWdMg.JPEG.chooddingg/PHOTO_0020.JPG?type=w800"
-                alt="#"
-              />
-
-              <div className={style.profile_img_edit}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  fill="white"
-                  class="bi bi-pencil-square"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                  <path
-                    fill-rule="evenodd"
-                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div> */}
-
-          {/* 크로퍼 */}
-
           <div className="cropper_content">
             <input
               type="file"
@@ -487,50 +458,6 @@ const SetInfo = () => {
                     <div className={style.setTable_userId}>
                       <div className={style.setTable_userId_originId}>
                         <p>{infoList.user_name}</p>
-
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-pencil-square"
-                          viewBox="0 0 16 16"
-                          onClick={onId}
-                        >
-                          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                          <path
-                            fill-rule="evenodd"
-                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                          />
-                        </svg>
-                      </div>
-                      <div className={style.setTable_userId_editId}>
-                        <form
-                          onSubmit={name_deit}
-                          className={onButton === "userId" ? style.active : ""}
-                        >
-                          <div class="input-group mb-3">
-                            <input
-                              type="text"
-                              class="form-control"
-                              aria-label="Sizing example input"
-                              aria-describedby="inputGroup-sizing-default"
-                              placeholder="새 이름"
-                              value={name1}
-                              onChange={(e) => setName1(e.target.value)}
-                              onBlur={namecheck}
-                            />
-                            <span>{nameCheckMsg}</span>
-                          </div>
-                          <div>
-                            <button
-                              type="submit"
-                              className={style.accept_button}
-                            >
-                              수정 완료
-                            </button>
-                          </div>
-                        </form>
                       </div>
                     </div>
                   </td>
